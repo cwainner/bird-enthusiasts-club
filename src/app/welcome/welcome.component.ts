08426a31fe62196fa2304c9d21f203d3e58e7f20
Chris Wainner <chris.jwainner@gmail.com>
2017-05-13 10:59:16 -0700

    Add recent bird sightings to welcome page as a tracker

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from "../member.model";
+import { MemberService } from "../member.service";
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+  styleUrls: ['./welcome.component.css'],
+  providers: [MemberService]
 })
 export class WelcomeComponent implements OnInit {
+  members;
 
-  constructor() { }
+  constructor(private memberService: MemberService) { }
 
   ngOnInit() {
+    this.members = this.memberService.getMembers();
   }
 
 }
